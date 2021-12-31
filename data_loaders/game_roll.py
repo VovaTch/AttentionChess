9e342d34e42c1d2cef9ea507611c5ead829a7a3e
Vladimir Tchuiev <rmcf126@gmail.com>
2021-12-31 20:29:17 +0200

    I think I worked out the kinks for the move generator. Fixed also some functionality of the conversion of promotion moves.

--- data_loaders/game_roll.py
+++ data_loaders/game_roll.py
@@ -2,6 +2,7 @@ import torch
 import chess
 import numpy as np
 import copy
+import random
 
 from model.attchess import AttChess
 from utils.util import board_to_tensor, legal_move_mask
@@ -11,12 +12,13 @@ class GameRoller:
     """A class for the model to play against itself. It plays a game and outputs the moves + result"""
 
     @torch.no_grad()
-    def __init__(self, model_good: AttChess, model_evil: AttChess, device='cuda', move_limit=150):
+    def __init__(self, model_good: AttChess, model_evil: AttChess, device='cuda', move_limit=300, argmax_start=300):
         """Loads the model"""
         self.model_good = copy.deepcopy(model_good)
         self.model_evil = copy.deepcopy(model_evil)
         self.device = device
         self.move_limit = move_limit
+        self.argmax_start = argmax_start
 
         self.board_buffer = []
         self.move_buffer = []
@@ -51,23 +53,43 @@ class GameRoller:
                 
                 # Check if we reached the endgame
                 move_num = current_node.moves_performed
-                if move_num + 1 >= 50:
+                if move_num + 1 >= self.argmax_start:
                     sample_move = False
                 else:
                     sample_move = True
                     
                 # Create node; append to new list only if the endgame wasn't reached
                 if init_flag:
+                    
                     new_node = init_node.sample_move(legal_move, quality_vec, sample_move=sample_move)
-                else:
-                    new_node = current_node.sample_move(legal_move, quality_vec, sample_move=sample_move)
-                    new_node.score_function.last_move_idx = move_num
+                    if not new_node.endgame_flag:
+                        new_nodes.append(new_node)
+                        board_list_new.append(new_node.board)
+                    else:
+                        new_node.propagate_score()
+                        
+                else: 
+                    while True:
+                        new_node = current_node.sample_move(legal_move, quality_vec, sample_move=sample_move)
+                        new_node.score_function.last_move_idx = move_num + 2  # The +2 hack is to prevent division by zero
+                        
+                        if not new_node.endgame_flag and move_num <= self.move_limit:  # TODO: once debugged get rid of the end condition
+                            new_nodes.append(new_node)
+                            board_list_new.append(new_node.board)
+                        else:
+                            print('Game ended')
+                            new_node.propagate_score()
+                        
+                        # Create random expansion
+                        random_num = random.uniform(0, 1)
+                        if random_num > expansion_constant:
+                            break
+                        else:
+                            print('New branch')
                 
-                if not new_node.endgame_flag:
-                    new_nodes.append(new_node)
-                    board_list_new.append(new_node.board)
-                else:
-                    new_node.propagate_score()
+                    
+            # After the initial roll, the next rolls are from the tree.
+            init_flag = False
                 
             current_nodes = new_nodes
             board_list = board_list_new
@@ -97,7 +119,7 @@ class BoardNode:
     """Create a board node for the MCTS.
     Include forward rolls for moves and backward rolls for quality vector updates. Score is based on length of game and the board"""
 
-    def __init__(self, board: chess.Board, parent, score_function, device='cuda'):
+    def __init__(self, board: chess.Board, parent, score_function, device='cuda', moves_performed=0):
 
         self.device = device
 
@@ -115,14 +137,15 @@ class BoardNode:
         self.board = board
         self.children = []
         self.endgame_flag, self.result = self._is_game_end()
-        self.moves_performed = 0
+        self.moves_performed = moves_performed
 
     def perform_move(self, move):
         """Get a move and create a child node"""
         board_copy = copy.deepcopy(self.board)
         board_copy.push(move)
-        new_child = BoardNode(board_copy, self, self.score_function, device=self.device)
+        new_child = BoardNode(board_copy, self, self.score_function, device=self.device, moves_performed=self.moves_performed)
         new_child.moves_performed += 1
+        print(f'MOves performed: {new_child.moves_performed}')
         self.children.append(new_child)
         return new_child
 
@@ -146,11 +169,13 @@ class BoardNode:
         if self.parent is None:
             return
 
+        turn_variable = 1 if self.board.turn is False else -1
         self.parent.score_function = self.score_function
         current_score = self.score_function(self.moves_performed, self.board)
         last_move = self.board.peek()
-        self.parent.quality_vector_logit[self.parent.quality_vector_logit ==
-                                         last_move] += current_score * self.result * self.board.turn  # TODO: Check if works
+        self.parent.result = self.result
+        quality_idx = [idx for idx, move in enumerate(self.parent.legal_move_list) if move == last_move]
+        self.parent.quality_vector_logit[quality_idx[0]] += current_score * self.result * turn_variable  # TODO: Check if works. The 1 replaced the result
         self.parent.propagate_score()  # Recursively apply the score propogation
 
     def flatten_tree(self):
@@ -158,11 +183,22 @@ class BoardNode:
         This is the most essential method, it prepares the data for learning."""
 
         # If there is no child, i.e. the game ended, return an empty stack. Otherwise, start collecting
-        board_list = list()
-        move_tensor = torch.zeros((0, 255)).to(self.device)
+        
+        move_tensor = torch.zeros((0, 256)).to(self.device)
 
         if len(self.children) == 0:
-            return board_list, move_tensor
+            print(f'Result: {self._is_game_end()}')
+            return [], move_tensor
+        else:
+            # board_list = [child.board for child in self.children if len(child.children) != 0]
+            board_list = [self.board]
+
+        turn_variable = 1 if self.board.turn is False else -1
+        
+        quality_vector_append = torch.zeros((1, 256)).to(self.device) - torch.inf
+        quality_vector_append[0, :self.quality_vector_logit.size()[0]] = self.quality_vector_logit
+        quality_vector_append[0, -1] = turn_variable * self.result
+        move_tensor = torch.cat((move_tensor, quality_vector_append))
 
         # Extract everything from the children nodes recursively
         for child in self.children:
@@ -187,10 +223,10 @@ class BoardNode:
 
 class ScoreWinFast:
 
-    def __init__(self, moves_to_end, score_max=10):
+    def __init__(self, moves_to_end, score_max=100):
         self.last_move_idx = moves_to_end
         self.score_max = score_max
 
     def __call__(self, move_idx, board: chess.Board):
-        score = self.score_max / (self.moves_to_end - move_idx)
+        score = self.score_max / (self.last_move_idx - move_idx)
         return score
--- test_stuff.py
+++ test_stuff.py
@@ -3,6 +3,7 @@ import chess.pgn
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 from utils.util import board_to_tensor, legal_move_mask, move_to_tensor, \
     board_to_embedding_coord, move_to_coordinate
@@ -17,9 +18,15 @@ def main():
 
     board = chess.Board()
     board_embed = board_to_embedding_coord(board)
-    loader = RuleAttentionChessLoader(batch_size=4, collate_fn=collate_fn)
-    attchess = AttChess()
+    loader = RuleAttentionChessLoader(batch_size=32, collate_fn=collate_fn)
+    attchess = AttChess(hidden_dim=32, num_heads=8, num_encoder=6, num_decoder=6, query_word_len=256, 
+                        num_chess_conv_layers=0, p_embedding=False)
     attchess = attchess.eval().to('cuda')
+    
+    # Load trained model
+    checkpoint = torch.load('model_best_init.pth')
+    attchess.load_state_dict(checkpoint['state_dict'])
+    
     move = chess.Move.from_uci('e2e4')
     coordinates = move_to_coordinate(move)
 
@@ -31,8 +38,8 @@ def main():
     board_data_loader = BoardEmbeddingLoader(batch_size=8)
     move_data_loader = MoveEmbeddingLoader(batch_size=8)
 
-    game_roller = GameRoller(model_good=attchess, model_evil=attchess)
-    game_roller.roll_game(board, num_of_branches=3)
+    game_roller = GameRoller(model_good=attchess, model_evil=attchess, move_limit=300)
+    game_roller.roll_game(board, num_of_branches=10  , expansion_constant=0.008)
 
     print(1111)
 
